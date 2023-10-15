d742778765ebcac99e11bf2dba19a66ccc744e7c
androidcode33 <androidcode33@gmail.com>
2023-10-15 11:36:22 +0300

    student, teacher editting, deletion

--- src/people/students.js
+++ src/people/students.js
@@ -1,11 +1,59 @@
 import '../App.css';
+import React, { useState } from 'react';
+import { apiFullCall } from '../helpers/apiHelper';
 
 const StudentsView = (props) => {
     const { students, setReload, reload } = props;
+    const [manageStudent, setManageStudent] = useState({ isOpen: false, student: {} });
+
+    const handleInputChange = e => {
+        // update the states
+        setManageStudent({
+            ...manageStudent,
+            student: {
+                ...manageStudent.student,
+                [e.target.name]: e.target.value
+            }
+        });
+    }
+
+    const handleSubmitUpdates = () => {
+        // its editting
+        if (manageStudent.student.id) {
+            apiFullCall(manageStudent.student, 'patch', `people/${manageStudent.student.id}/`).then((response) => {
+                if (response) {
+                    // Update the list with reload
+                    setReload(!reload);
+                }
+            }).catch((error) => error);
+
+        } else {
+            // its new student
+            apiFullCall(manageStudent.student, 'post', `people/`).then((response) => {
+                if (response) {
+                    apiFullCall({ username: manageStudent.student.username, student_people: response.id }, 'post', `students/`).then((studentResponse) => {
+                        if (studentResponse) {
+                            setReload(!reload);
+                        }
+                    }).catch((error) => error);
+                }
+            }).catch((error) => error);
+        }
+    }
+
+    const handleDeletion = (peopleId) => {
+        // deleting from people, deletes cascade
+        apiFullCall({}, 'delete', `people/${peopleId}/`).then((response) => {
+            if (response) {
+                // Update the list with reload
+                setReload(!reload);
+            }
+        }).catch((error) => error);
+    }
 
     return (
         <>
-            <h4>List Of Students <button>Add New</button> </h4>
+            <h4>List Of Students <button onClick={() => setManageStudent({ ...manageStudent, isOpen: true })}>Add New</button> </h4>
             <table>
                 <tr>
                     <th>#</th>
@@ -18,11 +66,23 @@ const StudentsView = (props) => {
                         <th>{index + 1}</th>
                         <th>{student.name}</th>
                         <th>{student.username}</th>
-                        <th><button>Edit</button><button>Delete</button></th>
+                        <th><button onClick={() => setManageStudent({ ...manageStudent, isOpen: true, student: student })}>Edit</button><button onClick={()=> handleDeletion(student.people_id)}>Delete</button></th>
                     </tr>
                 )}
 
             </table>
+
+            <dialog open={manageStudent.isOpen}>
+                <p>{manageStudent.student && manageStudent.student.id ? `Edit Student` : `Add New Student`}</p>
+                <form>
+                    <label for="name">Name:</label>
+                    <input type="text" id="fname" name="name" onChange={handleInputChange} value={manageStudent.student ? manageStudent.student.name : ``} /><br />
+                    <label for="surname">Surname:</label>
+                    <input type="text" id="surname" onChange={handleInputChange} name="surname" value={manageStudent.student ? manageStudent.student.surname : ``} /> <br />
+                    <button onClick={() => handleSubmitUpdates()}>Submit</button>
+                </form>
+            </dialog>
+
         </>
     );
 }
--- src/people/teachers.js
+++ src/people/teachers.js
@@ -1,11 +1,59 @@
 import '../App.css';
+import React, { useState } from 'react';
+import { apiFullCall } from '../helpers/apiHelper';
 
 const TeachersView = (props) => {
     const { teachers, setReload, reload } = props;
 
+    const [manageTeacher, setManageTeacher] = useState({ isOpen: false, teacher: {} });
+
+    const handleInputChange = e => {
+        // update states
+        setManageTeacher({
+            ...manageTeacher,
+            teacher: {
+                ...manageTeacher.teacher,
+                [e.target.name]: e.target.value
+            }
+        });
+    }
+
+    const handleSubmitUpdates = () => {
+        if (manageTeacher.teacher.id) {
+            // update teacher details
+            apiFullCall(manageTeacher.teacher, 'patch', `people/${manageTeacher.teacher.id}/`).then((response) => {
+                if (response) {
+                    // Update the list with reload
+                    setReload(!reload);
+                }
+            }).catch((error) => error);
+        } else {
+            // its new teacher
+            apiFullCall(manageTeacher.teacher, 'post', `people/`).then((response) => {
+                if (response) {
+                    apiFullCall({ teacher_people: response.id }, 'post', `teachers/`).then((teacherResponse) => {
+                        if (teacherResponse) {
+                            setReload(!reload);
+                        }
+                    }).catch((error) => error);
+                }
+            }).catch((error) => error);
+        }
+    }
+
+    const handleDeletion = (peopleId) => {
+        // deleting from people, deletes cascade
+        apiFullCall({}, 'delete', `people/${peopleId}/`).then((response) => {
+            if (response) {
+                // Update the list with reload
+                setReload(!reload);
+            }
+        }).catch((error) => error);
+    }
+
     return (
         <>
-            <h4>List Of Teachers <button>Add New</button> </h4>
+            <h4>List Of Teachers <button onClick={() => setManageTeacher({ ...manageTeacher, isOpen: true })}>Add New</button> </h4>
             <table>
                 <tr>
                     <th>#</th>
@@ -18,11 +66,20 @@ const TeachersView = (props) => {
                         <th>{index + 1}</th>
                         <th>{teacher.name}</th>
                         <th></th>
-                        <th><button>Edit</button><button>Delete</button></th>
+                        <th><button onClick={() => setManageTeacher({ ...manageTeacher, isOpen: true, teacher: teacher })}>Edit</button><button onClick={()=> handleDeletion(teacher.people_id)}>Delete</button></th>
                     </tr>
                 )}
 
             </table>
+
+            <dialog open={manageTeacher.isOpen}>
+                <p>{manageTeacher.teacher && manageTeacher.teacher.id ? `Edit Student` : `Add New Student`}</p>
+                <form>
+                    <label for="name">Name:</label>
+                    <input type="text" id="fname" name="name" onChange={handleInputChange} value={manageTeacher.teacher ? manageTeacher.teacher.name : ``} /><br />
+                    <button onClick={() => handleSubmitUpdates()}>Submit</button>
+                </form>
+            </dialog>
         </>
     );
 }
